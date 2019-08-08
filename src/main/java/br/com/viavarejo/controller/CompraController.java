3700b07f436cac2ca87ce24a9ac580005a45f7ff
Lucas Geovanni <lucasgeovanni1995@gmail.com>
2019-08-08 12:07:11 -0300

    adionando controller, e swagger

---
+++ src/main/java/br/com/viavarejo/controller/CompraController.java
@@ -0,0 +1,31 @@
+package br.com.viavarejo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.viavarejo.models.Compra;
+import br.com.viavarejo.models.Parcela;
+import br.com.viavarejo.service.ICompraService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@Api(value = "compra")
+public class CompraController {
+
+	@Autowired
+	private ICompraService compraService;
+
+	@ApiOperation(value = "Criar uma nova compra")
+	@PostMapping(value = "/compra", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<List<Parcela>> get(@RequestBody Compra compra) {
+		return ResponseEntity.ok(compraService.processaCompra(compra));
+	}
+
+}
